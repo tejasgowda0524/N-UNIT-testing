5b30fd81546bbd4860e4d011670a05b820a11ce5
TEJAS <tejasgowda0524@gmail.com>
2024-03-04 23:33:13 +0530

    ,,,

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace demo1
+{
+    public class Program
+    {
+        public int addition(int a,int b)
+        {
+            return a + b;
+        }
+        public int sub(int a,int b)
+        {
+            return a-b;
+        }
+    }
+    public class main
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using demo1;
+namespace demo21
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 30;
+            int b = 20;
+            int c = a+b;
+            int d = a-b;
+            int expected = 30+20;
+            int expexted2 = 30-20;
+            var val = new Program();
+            int m = val.addition(a, b);
+            Assert.AreEqual(expected, c);
+
+        }
+    }
+}
